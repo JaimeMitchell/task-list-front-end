cba238e6f78f8312ab2b44589f5b1118bc3f4aac
Aria Stroud <stroudaria@gmail.com>
2022-12-19 19:21:21 -0800

    Updates App to display tasks from backend. Toggle button does not update in backend yet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
+
+// aria notes: displays tasks from back end, but toggle does not update in back end yet.
+const kBaseUrl = 'http://localhost:5000';
+
+const convertFromApi = (apiTask) => {
+  const { description, id, is_complete: isComplete, title } = apiTask;
+
+  const newTask = { description, id, isComplete, title };
+  return newTask;
+};
+
+const getAllTasksApi = () => {
+  return axios
+    .get(`${kBaseUrl}/tasks`)
+    .then((response) => {
+      return response.data.map(convertFromApi);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 const App = () => {
-  const [tasks, setTasks] = useState(TASKS);
+  // const [tasks, setTasks] = useState(TASKS);
+  const [tasks, setTasks] = useState([]);
+
+  const getAllTasks = () => {
+    return getAllTasksApi().then((tasks) => {
+      console.log(tasks);
+      setTasks(tasks);
+    });
+  };
+
+  useEffect(() => {
+    getAllTasks();
+  }, []);
 
   const toggleButton = (id) => {
     setTasks((tasks) =>
